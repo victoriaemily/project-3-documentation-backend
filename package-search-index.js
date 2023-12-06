7abdb97e583661c5b9c5ac819d6e0123667b4b21
vakiko <victoria.c6140@gmail.com>
2023-12-05 23:28:40 -0600

    documentation for BE

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example"},{"l":"com.example.config"},{"l":"com.example.controller"},{"l":"com.example.model"},{"l":"com.example.repository"}];updateSearchResults();
